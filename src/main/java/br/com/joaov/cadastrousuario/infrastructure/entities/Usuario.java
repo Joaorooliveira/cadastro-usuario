7beb15b6e40477cfc8b2c031a7f751a13ed23065
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-04 17:26:10 -0300

    feat(entity-usuario): adiciona entidade usuario

---
+++ src/main/java/br/com/joaov/cadastrousuario/infrastructure/entities/Usuario.java
@@ -0,0 +1,26 @@
+package br.com.joaov.cadastrousuario.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name ="usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "email",unique = true) // Quer dizer q o nome da tabela e email e esse atributo e unico
+    private String email;
+
+    @Column(name= "nome")
+    private String nome;
+
+
+}
