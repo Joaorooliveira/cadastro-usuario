74c2cd81584cd6d8b35ad2b7af28516da8e4514d
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-04 23:11:39 -0300

    feat(usuario-controller): adiciona endpoint para salvar usuario

---
+++ src/main/java/br/com/joaov/cadastrousuario/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package br.com.joaov.cadastrousuario.controller;
+
+import br.com.joaov.cadastrousuario.business.UsuarioService;
+import br.com.joaov.cadastrousuario.infrastructure.entities.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+
+    private final UsuarioService usuarioService;
+
+
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario) {
+        usuarioService.salvarUsuario(usuario);
+        return ResponseEntity.ok().build();
+    }
+}
