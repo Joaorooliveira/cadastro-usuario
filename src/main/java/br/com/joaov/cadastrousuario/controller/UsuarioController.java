67f27ff8c3756e8013f3c9930dd667c41c28ba8c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-04 23:37:13 -0300

    feat(usuario-controller): adiciona novo endpoint para atualizar usuario por id

--- src/main/java/br/com/joaov/cadastrousuario/controller/UsuarioController.java
+++ src/main/java/br/com/joaov/cadastrousuario/controller/UsuarioController.java
@@ -35,4 +35,11 @@ public class UsuarioController {
        usuarioService.deletarUsuarioPorEmail(email);
        return ResponseEntity.ok().build();
     }
+
+    @PutMapping()
+    @Transactional
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Long id ,@RequestBody Usuario usuario) {
+        usuarioService.atualizarUsuarioPorId(id, usuario);
+        return ResponseEntity.ok().build();
+    }
 }
